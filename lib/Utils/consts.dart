6bc389ac77d71fd4525b237282f1369acd67138f
Raffashe <raffacst@outlook.com>
2023-06-22 13:30:41 -0300

    feat: :lipstick: Adding the color variables
    Adding the color variables

--- lib/Utils/consts.dart
+++ lib/Utils/consts.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
-class Consts extends StatelessWidget {
-  const Consts({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+class ConstColors {
+  Color primary100 = const Color.fromRGBO(228, 220, 207, 100);
+  Color primary200 = const Color.fromRGBO(240, 235, 2227, 100);
+  Color primary300 = const Color.fromRGBO(63, 91, 98, 100);
+  Color accent100 = const Color.fromRGBO(214, 153, 85, 100);
+  Color accent200 = const Color.fromRGBO(248, 218, 168, 100);
+  Color bg100 = const Color.fromRGBO(87, 111, 114, 100);
+  Color bg200 = const Color.fromRGBO(125, 157, 156, 100);
+  Color bg300 = const Color.fromRGBO(140, 169, 170, 100);
+  Color txt100 = const Color.fromRGBO(238, 238, 238, 100);
+  Color txt200 = const Color.fromRGBO(220, 220, 220, 100);
 }
